faba0bdb570a84d8687734fce0667def241e8654
Daniil Ryazanov <kein@tagproject.ru>
2019-06-07 00:12:50 +0300

    test: modify test for Hook and UpdateManager
    tash: #z3emz
    
    Signed-off-by: Daniil Ryazanov <kein@tagproject.ru>

--- test/hook.test.ts
+++ test/hook.test.ts
@@ -12,32 +12,58 @@ const stream: any = {
     testMessage(): string {
         return `message ${++this.messagesCount}`;
     },
+    getBuffer(str: string): Buffer {
+        return Buffer.from(str, 'utf8');
+    },
+    getArrayBuffer(str: string): Uint8Array {
+        return new Uint8Array(Buffer.from(this.getBuffer(str), 'utf8'));
+    },
     write(str: string, cb?: () => void): boolean {
-        console.log(arguments);
         if (cb) cb();
 
         return !!output.push(str);
     },
 };
 const hook = new Hook(stream);
+let msg1: string;
+let msg2: string;
 
-it('Hook', (): void => {
-    hook.active();
-    expect(output.pop()).toBe(ansiEscapes.cursorHide);
+describe('Hook', (): void => {
+    it('Active', (): void => {
+        hook.active();
+        expect(output.pop()).toBe(ansiEscapes.cursorHide);
+    });
 
-    hook.write(stream.testMessage());
-    expect(output.length).toBe(stream.messagesCount);
+    it('Write', (): void => {
+        hook.write(stream.testMessage());
+        expect(output.length).toBe(stream.messagesCount);
 
-    hook.clear(stream.messagesCount);
-    expect(output.pop()).toBe(ansiEscapes.eraseLines(stream.messagesCount + 1));
+        hook.clear(stream.messagesCount);
+        expect(output.pop()).toBe(ansiEscapes.eraseLines(stream.messagesCount + 1));
 
-    stream.write(stream.testMessage(), stream.testCallback);
-    hook.write(stream.testMessage());
-    expect(output.length).toBe(stream.messagesCount - 1);
-    expect(stream.callbacksCount).toBe(0);
+        stream.write(stream.testMessage(), stream.testCallback);
+        hook.write(stream.testMessage());
+        expect(output.length).toBe(stream.messagesCount - 1);
+        expect(stream.callbacksCount).toBe(1);
+    });
 
-    hook.inactive();
-    expect(output.pop()).toBe(ansiEscapes.cursorShow);
-    expect(output.length).toBe(stream.messagesCount);
-    expect(stream.callbacksCount).toBe(1);
+    it('Write (others)', (): void => {
+        stream.write(stream.testMessage(), 'utf8', stream.testCallback);
+        expect(stream.callbacksCount).toBe(2);
+
+        stream.write(stream.getBuffer((msg1 = stream.testMessage())), stream.testCallback);
+        expect(stream.callbacksCount).toBe(3);
+
+        stream.write(stream.getArrayBuffer((msg2 = stream.testMessage())), stream.testCallback);
+        expect(stream.callbacksCount).toBe(4);
+    });
+
+    it('Inactive', (): void => {
+        hook.inactive();
+
+        expect(output.pop()).toBe(ansiEscapes.cursorShow);
+        expect(output.length).toBe(stream.messagesCount);
+        expect(output.pop()).toBe(msg2);
+        expect(output.pop()).toBe(msg1);
+    });
 });
