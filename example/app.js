e4c654e828a7619a03470b3038a461b9ac3dea61
Daniil Ryazanov <kein@tagproject.ru>
2019-06-07 01:22:15 +0300

    chore: add demo.gif, refactor example
    Signed-off-by: Daniil Ryazanov <kein@tagproject.ru>

--- example/app.js
+++ example/app.js
@@ -1,23 +1,27 @@
 const { UpdateManager } = require('../lib/update-manager');
 const manager = UpdateManager.getInstance();
 
+const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+const messages = ['Swapping time and space...', 'Have a good day.', "Don't panic...", 'Updating Updater...', '42'];
+let i = (j = 0);
+let number = 60;
+
 manager.hook();
+
 console.log(' - log message');
 console.error(' - error message');
 console.warn(' - warn message');
 
-const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
-let i = 0;
-let number = 30;
-
 const id = setInterval(() => {
     if (--number < 0) {
         clearInterval(id);
+        manager.update(['✔ Success']);
         manager.update(['', 'Messages:'], 1);
         manager.unhook();
     } else {
         const frame = frames[(i = ++i % frames.length)];
+        const message = messages[(j = Math.round(number / 10) % messages.length)];
 
-        manager.update([`${frame} some process...`, '--- end ---']);
+        manager.update([`${frame} Some process...`, message]);
     }
 }, 80);
