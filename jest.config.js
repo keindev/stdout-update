0b87e4b54d6b34c8bb058801850d6248a8bd4b5f
keindev <kein@tagproject.ru>
2021-03-03 02:00:32 +0300

    build: add github actions

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageReporters: ['text-summary', 'json'],
+  coverageReporters: ['text-summary', 'lcov'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/lib/', '/node_modules/'],
