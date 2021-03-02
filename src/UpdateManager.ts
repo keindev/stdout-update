0cc33724744497f0353e27c4d28b8de334e26f8e
keindev <kein@tagproject.ru>
2021-03-02 23:58:32 +0300

    refactor: refactor modules

---
+++ src/UpdateManager.ts
@@ -0,0 +1,121 @@
+import { Hook } from './Hook';
+import { Terminal } from './Terminal';
+import { Wrapper } from './Wrapper';
+
+export class UpdateManager {
+  private static instance?: UpdateManager;
+  #hooks: Hook[];
+  #wrapper: Wrapper;
+  #terminal: Terminal;
+  #lastLength = 0;
+  #outside = 0;
+  #isActive = false;
+
+  private constructor(stdout: NodeJS.WriteStream, stderr: NodeJS.WriteStream) {
+    this.#hooks = [stdout, stderr].map((stream): Hook => new Hook(stream));
+    this.#terminal = new Terminal(stdout);
+    this.#wrapper = new Wrapper();
+  }
+
+  /**
+   * Method to get the object to control the streams update
+   * @param stdout - [process.stdout](https://nodejs.org/api/process.html#process_process_stdout)
+   * @param stderr - [process.stderr](https://nodejs.org/api/process.html#process_a_note_on_process_i_o)
+   */
+  static getInstance(
+    stdout: NodeJS.WriteStream = process.stdout,
+    stderr: NodeJS.WriteStream = process.stderr
+  ): UpdateManager {
+    if (!UpdateManager.instance) {
+      UpdateManager.instance = new UpdateManager(stdout, stderr);
+    }
+
+    return UpdateManager.instance;
+  }
+
+  /**
+   * Last printed rows count
+   */
+  get lastLength(): number {
+    return this.#lastLength;
+  }
+
+  /**
+   * Rows count outside editable area
+   */
+  get outside(): number {
+    return this.#outside;
+  }
+
+  /**
+   * Hook activity status
+   */
+  get isHooked(): boolean {
+    return this.#isActive;
+  }
+
+  /**
+   * Hook stdout and stderr streams
+   *
+   * @returns Success status
+   */
+  hook(): boolean {
+    if (!this.#isActive) {
+      this.#hooks.forEach((hook) => hook.active());
+      this.clear(true);
+    }
+
+    return this.#isActive;
+  }
+
+  /**
+   * Unhooks both stdout and stderr streams and print their story of logs
+   * @param separateHistory - If `true`, will add an empty line to the history output for individual recorded lines and console logs
+   * @returns Success status
+   */
+  unhook(separateHistory = true): boolean {
+    if (this.#isActive) {
+      this.#hooks.forEach((hook) => hook.inactive(separateHistory));
+      this.clear();
+    }
+
+    return !this.#isActive;
+  }
+
+  /**
+   * Update output
+   * @param rows - Text lines to write to standard output
+   * @param from - Index of the line starting from which the contents of the terminal are being overwritten
+   */
+  update(rows: string[], from = 0): void {
+    if (rows.length) {
+      const [hook] = this.#hooks;
+      const height = this.#terminal.getHeight();
+      const width = this.#terminal.getWidth();
+      const position = from > height ? height - 1 : Math.max(0, Math.min(height - 1, from));
+      const actualLength = this.lastLength - position;
+      const outside = Math.max(actualLength - height, this.outside);
+      let output = rows.reduce<string[]>((acc, row) => acc.concat(this.#wrapper.wrap(row, width)), []);
+
+      if (height <= actualLength) {
+        hook.clear(height);
+
+        if (position < outside) {
+          output = output.slice(outside - position + 1);
+        }
+      } else if (actualLength) {
+        hook.clear(actualLength);
+      }
+
+      hook.write(output.join(Terminal.EOL) + Terminal.EOL);
+      this.#lastLength = outside ? outside + output.length + 1 : output.length;
+      this.#outside = Math.max(this.lastLength - height, this.outside);
+    }
+  }
+
+  private clear(status = false): void {
+    this.#isActive = status;
+    this.#lastLength = 0;
+    this.#outside = 0;
+  }
+}
---
+++ src/Wrapper.ts
@@ -0,0 +1,134 @@
+import ansiStyles from 'ansi-styles';
+import stringWidth from 'string-width';
+import stripAnsi from 'strip-ansi';
+
+import { Terminal } from './Terminal';
+
+const ESCAPES = new Set(['\u001B', '\u009B']);
+
+const wrapEscapes = (characters: string): string => {
+  const slice = (index: number): RegExpExecArray | null => /\d[^m]*/.exec(characters.slice(index, index + 4));
+  const wrap = (code: number): string => `${ESCAPES.values().next().value}[${code}m`;
+  let result = '';
+  let match: RegExpExecArray | null;
+  let code: number | undefined;
+  let escapeCode: number | null;
+
+  [...characters].forEach((character, index): void => {
+    result += character;
+
+    if (ESCAPES.has(character)) {
+      match = slice(index);
+
+      if (match) {
+        code = parseFloat(match[0]);
+        escapeCode = code === 39 ? null : code;
+      }
+    }
+
+    code = ansiStyles.codes.get(Number(escapeCode));
+
+    if (escapeCode && code) {
+      if (characters[index + 1] === Terminal.EOL) {
+        result += wrap(code);
+      } else if (character === Terminal.EOL) {
+        result += wrap(escapeCode);
+      }
+    }
+  });
+
+  return result;
+};
+
+const trimRight = (str: string): string => {
+  const words = str.split(' ');
+  let last = words.length;
+
+  while (last > 0 && stringWidth(words[last - 1]) <= 0) last--;
+
+  return last === words.length ? str : words.slice(0, last).join(' ') + words.slice(last).join('');
+};
+
+export class Wrapper {
+  #rows: string[];
+
+  public constructor() {
+    this.#rows = [''];
+  }
+
+  public wrap(str: string, limit: number): string[] {
+    if (!str.trim().length) return [''];
+
+    this.#rows = [''];
+
+    const rows = this.#rows;
+    const words = str.normalize().split(' ');
+    const lengths = words.map((character): number => stringWidth(character));
+    let rowLength: number;
+    let wordLength: number;
+
+    words.forEach((word, index): void => {
+      rowLength = stringWidth(rows[rows.length - 1]);
+      wordLength = lengths[index];
+
+      if (index !== 0) {
+        rows[rows.length - 1] += ' ';
+        rowLength++;
+      }
+
+      if (wordLength > limit) {
+        const remainingColumns = limit - rowLength;
+        const breaksStartingThisLine = 1 + Math.floor((wordLength - remainingColumns - 1) / limit);
+        const breaksStartingNextLine = Math.floor((wordLength - 1) / limit);
+
+        if (breaksStartingNextLine < breaksStartingThisLine) rows.push('');
+
+        this.wrapWord(word, limit);
+      } else {
+        if (rowLength + wordLength > limit && rowLength && wordLength) rows.push('');
+
+        rows[rows.length - 1] += word;
+      }
+    });
+
+    return wrapEscapes(rows.map(trimRight).join(Terminal.EOL)).split(Terminal.EOL);
+  }
+
+  private wrapWord(word: string, limit: number): void {
+    const rows = this.#rows;
+    const characters = [...word];
+    let isInsideEscape = false;
+    let visible = stringWidth(stripAnsi(rows[rows.length - 1]));
+    const lineBreak = (l: number, i: number): void => {
+      visible += l;
+
+      if (visible === limit && i < characters.length - 1) {
+        rows.push('');
+        visible = 0;
+      }
+    };
+
+    characters.forEach((character, index): void => {
+      const characterLength = stringWidth(character);
+
+      if (visible + characterLength <= limit) {
+        rows[rows.length - 1] += character;
+      } else {
+        rows.push(character);
+        visible = 0;
+      }
+
+      if (ESCAPES.has(character)) {
+        isInsideEscape = true;
+      } else if (isInsideEscape && character === 'm') {
+        isInsideEscape = false;
+      } else if (!isInsideEscape) {
+        lineBreak(characterLength, index);
+      }
+    });
+
+    if (!visible && rows[rows.length - 1].length > 0 && rows.length > 1) {
+      rows[rows.length - 2] += rows.pop();
+    }
+  }
+}
